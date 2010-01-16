3723722995b73a9e729f80ae351f1d328b5ed5a3
atla <koerner.marcus@gmail.com>
2010-01-16 17:40:06 +0100

    added function combineItinerariesIntoOne and implemented searchMultisegment
    not yet finished, delivers zero results, but the code should be more or less what i wrote, please review and fix ;)

--- src/main/scala/lufthansa/lufthansa.scala
+++ src/main/scala/lufthansa/lufthansa.scala
@@ -37,18 +37,18 @@ package lufthansa {
             	// Get all flights that are possible after having taken the given flight en route to the destination whereas some Airports
             	// are already visited and shouldn't be considered anymore.
                 def getFlightsStartingWith (flight: Flight, visited: List[Airport], destination: Airport) : List[List[Flight]] = {
-                	// Check the reached destination by the current flight
+                    // Check the reached destination by the current flight
                     flight.destination match {
 
                         // reached target airport, return the flights
                         case dest if (dest == destination) => List(flight) :: Nil
 
-                         // try recursive find
+                            // try recursive find
                         case _ if (visited.contains (flight.destination) == false) => {
 
                                 // match for max hops
                                 visited.length match {
-                                	// Are we inside our range?
+                                    // Are we inside our range?
                                     case hops if (hops - 1 < maxHops) => {
 
                                             // search only for flights after landing ... in future we should also support some transit duration
@@ -62,18 +62,18 @@ package lufthansa {
                                             getFlightsTo (flightsNew, flight.destination :: visited, destination, maxHops, maxReachDestinationDate) map ( flights => flight :: flights )
                                         }
 
-                                    // did not reach destination Airport with given hops
+                                        // did not reach destination Airport with given hops
                                     case _ => Nil
                                 }
                             }
-                        // We didn't find a satisfying flight
+                            // We didn't find a satisfying flight
                         case _ => Nil
                     }
                 }
 
                 // find list of flights to the destination airport (segments) for all flights starting at the origin airport
                 flights match {
-                	// Walk through the algorithm
+                    // Walk through the algorithm
                     case hd :: tl => getFlightsStartingWith (hd, visited, destination) ::: getFlightsTo (tl, visited, destination, maxHops, maxReachDestinationDate)
                     case _ => Nil
                 }
@@ -108,23 +108,114 @@ package lufthansa {
         // TODO implement using searchOneway
         override def searchRoundtrip(origin: world.Airport, destination: world.Airport, departureDate: Date, returnDate: Date): Seq[Itinerary] = {
             
-    		// find Itineraries in both directions
-    		val to = searchOneway(origin, destination, departureDate)
-    		val back = searchOneway(destination, origin, returnDate)
-          
-    		// Combine to and back Itineraries
-    		for(p1 <- to){
-    		  
-    		}
+//            // find Itineraries in both directions
+//            val to = searchOneway(origin, destination, departureDate)
+//            val back = searchOneway(destination, origin, returnDate)
+//
+//            // Combine to and back Itineraries
+//            for(p1 <- to){
+//
+//            }
       
-    		Nil
+            // this is just a special case of searchMultisegment
+            searchMultisegment (List((origin, destination, departureDate), (destination, origin, returnDate)))
+
+
+            //Nil
         }
 
-        // Get all Itineraries that let you travel between a sequence of airport pairs on specific dates
-        // TODO implement this using the searchOneway
-        override def searchMultisegment(trips: Seq[(world.Airport, world.Airport, Date)]): Seq[Itinerary] = {
-            Nil
-        }
+        /*
 
-    }
-}
+
+
+
+         An itinerary is a series of segments of flights. It is the final entity passengers get and pay for.
+
+         For a one-way direct flight, it contains a single segment, which contains a single hop.
+         For a one-way connect flight, it  contains a single segment, which contains multiple hops.
+         For a round-trip flight, it contains two segments.
+         For a multisegment flight, it contains at least two segments.
+
+         For example, a multisegment itinerary would be:
+         Segment 1: from Chengdu via Beijing, Amsterdam to Frankfurt, then stay half a year
+         Hop 1.1: Chengdu -> Beijing
+         Hop 1.2: Beijing -> Amsterdam
+         Hop 1.3: Amsterdam -> Frankfurt
+
+         Segment 2: from Frankfurt via Toronto to St. John's, then stay half a year
+         Hop 2.1: Frankfurt -> Toronto
+         Hop 2.2: Toronto -> St. John's
+
+         Segment 3: from St. John's via Toronto, Vancouver, Beijing to Chengdu
+         Hop 3.1: St. John's -> Toronto
+         Hop 3.2: Toronto -> Vancouver
+         Hop 3.3: Vancouver -> Beijing
+         Hop 3.4: Beijing -> Chengdu
+
+         //val id: String    // globally unique identifier for a particular intinerary; useful for later booking
+         //val departureDate: Date // departureDate of the first hop
+         //val duration: Int // time interval in minutes; = sum of duration for all segments
+         //val origin: Airport       // origin airport
+         //val destination: Airport  // ultimate destination airport
+         //val segments: Seq[Segment]
+         //val price: Int    // amount of ticket fare + tax (e.g. dollars)
+
+
+
+         val hops = for (flight <- flightseq) yield flight.toHop
+
+         // calc sum of durations
+         val singleDurations : List [Int] = flightseq.map (flight => flight.duration)
+         val sumOfDurations = singleDurations.foldLeft(0)((a,b) => a+b)
+
+         // create segment for itinerary
+         val seg = new Segment (origin, destination, hops.head.departureDate, sumOfDurations, hops)
+
+         new Itinerary ("newIt", seg.departureDate, seg.duration, origin, destination, seg :: Nil, 100)
+
+         */
+
+         private def combineItinerariesIntoOne (itineraries : List[Itinerary]) : Itinerary = {
+
+                def collectSegments (lst:List[Itinerary]) : List[Segment] = lst match {
+                    case Nil => Nil
+                    case hd :: tl => hd.segments.toList ::: collectSegments (tl)
+                }
+
+                val allSegments : List[Segment] = collectSegments (itineraries)
+                val departureDate = allSegments.head.departureDate
+
+                val origin = allSegments.head.origin
+                val destination = allSegments.last.destination
+
+                val allDurations : List[Int] = allSegments.map (seg => seg.duration)
+                val sumOfSegmentDurations = allDurations.foldLeft (0)((a,b) => a+b)
+
+                new Itinerary ("newIt", departureDate, sumOfSegmentDurations, origin, destination, allSegments, 100)
+            }
+
+         // Get all Itineraries that let you travel between a sequence of airport pairs on specific dates
+         // DISCLAIMER: using searchOneway for this implementation might not be the best speed we can achieve
+         // still the most time consuming thing will the combinatorial explosion if there are lots of itineraries found
+         // for the single trips
+         override def searchMultisegment(trips: Seq[(world.Airport, world.Airport, Date)]): Seq[Itinerary] = {
+
+                def combineItineraries (flights : List[List[Itinerary]]) : List [List[Any]] = flights map (
+                    its => flights match {
+                        case Nil => Nil
+                        case hd :: tl => its flatMap ( it => List(it) ::: combineItineraries (tl))
+                    }
+                )
+
+
+
+                val allFlights : Seq[Seq[Itinerary]] = trips map (trip => searchOneway (trip._1, trip._2, trip._3))
+                val allFlightsList = (allFlights map (s1 => s1.toList)).toList
+
+                val allFlightsCombined : List[List[Itinerary]] = combineItineraries (allFlightsList).asInstanceOf[List[List[Itinerary]]]
+
+                allFlightsCombined.map (it => combineItinerariesIntoOne (it))
+            }
+
+         }
+         }
